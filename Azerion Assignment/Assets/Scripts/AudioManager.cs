6d5affca947ee84e297ff6f019562579abe00211
Leunam <leunam.san@gmail.com>
2020-06-11 12:39:34 +0200

    [Code+Scene+Audio] - AudioManager created, added to scene + song file

---
+++ Azerion Assignment/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource musicPlayer;
+
+    static AudioManager instance;
+
+    public static AudioManager Instance { get { return instance; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        if (musicPlayer)
+            musicPlayer.Play();
+    }
+
+}
