b65aee12bf0981e043ff3c43227ccac9b8fe1850
Leunam <leunam.san@gmail.com>
2020-06-14 01:09:19 +0200

    [Prefab+Code] - Bullet prefab created + code to rotate bullets

---
+++ Azerion Assignment/Assets/Scripts/RotateMe.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple script to rotate objects
+/// </summary>
+public class RotateMe : MonoBehaviour
+{
+    public float rotationSpeed;
+    public bool anticlockwise;
+
+    private float direction;
+
+    void Start()
+    {
+        if (anticlockwise)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+        }
+
+        rotationSpeed = rotationSpeed * direction;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}
