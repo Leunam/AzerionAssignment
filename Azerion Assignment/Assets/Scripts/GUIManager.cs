1fa30ed3224ac24de7cd3355f4f6e418f6c88a2f
Leunam <leunam.san@gmail.com>
2020-06-14 00:35:08 +0200

    [Code] - GUIManager, initial script
    Button_Play method used on MainMenu to Play Level_01 scene

---
+++ Azerion Assignment/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    public void Button_Play()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+}
