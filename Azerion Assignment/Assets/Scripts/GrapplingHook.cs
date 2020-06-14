e5c5b068fc4873d2ea7febba1674a54729424715
Leunam <leunam.san@gmail.com>
2020-06-14 16:07:05 +0200

    [Code] - Grappling hook checking Joystick angle

--- Azerion Assignment/Assets/Scripts/GrapplingHook.cs
+++ Azerion Assignment/Assets/Scripts/GrapplingHook.cs
@@ -10,6 +10,11 @@ public class GrapplingHook : MonoBehaviour
     public float distance = 10f;
     public LayerMask mask;
     public float step = 0.2f;
+    public Joystick joystick;
+
+    private bool hookPressed = false;
+
+
 
     void Start()
     {
@@ -17,6 +22,18 @@ public class GrapplingHook : MonoBehaviour
         joint.enabled = false;
         line.enabled = false;
     }
+
+    public void HookButtonDown()
+    {
+        hookPressed = true;
+    }
+
+    public void HookButtonUp()
+    {
+        hookPressed = false;
+    }
+
+
 
     void Update()
     {
@@ -29,18 +46,23 @@ public class GrapplingHook : MonoBehaviour
         //    joint.enabled = false;
 
         //}
+        float joystickMagnitude = joystick.Direction.magnitude;
+        //Debug.Log("joystickMagnitude > " + joystickMagnitude.ToString());
 
+        float angleToJoystick = Mathf.Atan2(joystick.Direction.x, joystick.Direction.y) ;
+        Debug.Log("Joystick Angle -> " + angleToJoystick);
 
-        if (Input.GetMouseButtonDown(0))
+        if (hookPressed)
         {
-            //Debug.Log("dentro");
+            Debug.Log("Hook button down");
             targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             targetPos.z = 0;
-            
-            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
 
-            //if (hit)
-                //Debug.DrawRay(transform.position, new Vector2(hit.transform.position.x, hit.transform.position.y));
+            //hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, mask);
+            hit = Physics2D.Raycast(transform.position, new Vector2(Mathf.Sin(angleToJoystick), Mathf.Cos(angleToJoystick)), distance, mask);
+
+            if (hit)
+                Debug.DrawRay(transform.position, new Vector2(hit.transform.position.x, hit.transform.position.y));
             
             if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
             {
@@ -68,17 +90,18 @@ public class GrapplingHook : MonoBehaviour
         }
         
 
-        if (Input.GetMouseButton(0))
-        {
+        //if (Input.GetMouseButton(0))
+        //{
 
-            line.SetPosition(0, transform.position);
-        }
+        //    line.SetPosition(0, transform.position);
+        //}
 
 
-        if (Input.GetMouseButtonUp(0))
+        if (!hookPressed)
         {
             joint.enabled = false;
             line.enabled = false;
+            Debug.Log("Hook button up");
         }
 
     }
