27c0064f6c3d3c4f78ac457f4b89437cd18d4d01
Leunam <leunam.san@gmail.com>
2020-06-15 11:35:24 +0200

    [Code] - Bullets controller + Bullets pooler added

---
+++ Azerion Assignment/Assets/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float bulletSpeed = 25; //Bullet Speed default value to 25, exposed on Inspector to tune it if needed.
+
+    void Update()
+    {
+        this.transform.position += Vector3.right * bulletSpeed * Time.deltaTime;
+    }
+
+    private void OnBecameInvisible()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Azerion Assignment/Assets/Scripts/BulletPooler.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BulletPooler : MonoBehaviour {
+
+
+    // ==============================
+    // New Method to Pool the Bullets
+    // ==============================
+    public static BulletPooler current;
+    public GameObject pooledBulletObject;
+    public int pooledBulletsAmount = 20;
+    public bool willGrow = true;
+
+    public List<GameObject> pooledBulletObjects;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+	void Start () {
+        // New method to pool the bullets.
+        // Create a new list of bullets with 'pooledBulletsAmount' bullets on it.
+        pooledBulletObjects = new List<GameObject>();
+        for (int i = 0; i < pooledBulletsAmount; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledBulletObject);
+            obj.transform.parent = current.transform;
+            obj.SetActive(false);
+            pooledBulletObjects.Add(obj);
+        }
+
+	}
+
+    public GameObject GetPooledObject(bool _backfire)
+    {
+        for (int i = 0; i < pooledBulletObjects.Count; i++)
+        {
+            if (!pooledBulletObjects[i].activeInHierarchy)
+            {
+                if(_backfire)
+                    pooledBulletObjects[i].transform.rotation = new Quaternion(0, 180, 0, 0);
+                else
+                    pooledBulletObjects[i].transform.rotation = new Quaternion(0, 0, 0, 0);
+                return pooledBulletObjects[i];
+            }
+        }
+        // If we need more bullets for whatever reason we expand the pool of bullets creating a new bullet and adding it to the pool.
+        if (willGrow)
+        {
+            GameObject obj = (GameObject)Instantiate(pooledBulletObject);
+            obj.transform.parent = current.transform;
+            pooledBulletObjects.Add(obj);
+            if (_backfire)
+                obj.transform.rotation = new Quaternion(0, 180, 0, 0);
+            else
+                obj.transform.rotation = new Quaternion(0, 0, 0, 0);
+            return obj;
+        }
+
+        return null;
+    }
+}
