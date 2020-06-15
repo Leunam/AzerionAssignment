9d9f827125c8fd380e7f1c5a0e3b695f00e4c534
Leunam <leunam.san@gmail.com>
2020-06-15 05:15:07 +0200

    [Scene+Code] - Level_01 Scene changes + AudioManager new Method
    Level_01 Scene:
    - AudioManager singleton moved to Hierarchy root
    - New Event Triggers for Fire button

--- Azerion Assignment/Assets/Scripts/AudioManager.cs
+++ Azerion Assignment/Assets/Scripts/AudioManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
+    [Header("AudioSources")]
     public AudioSource musicPlayer;
+    public AudioSource sfxPlayer;
 
+    [Header("Audio FX")]
+    public AudioClip bulletShotSFX;
+
+    //Singleton creation statements.
     static AudioManager instance;
-
     public static AudioManager Instance { get { return instance; } }
 
-    //Manage instance, so we don't have duplicate instance in the scene when loaded.
+    //Manage instance, so we don't have a duplicate instance in the scene when loaded.
     private void Awake()
     {
         if (instance == null)
@@ -31,6 +35,11 @@ public class AudioManager : MonoBehaviour
     {
         if (musicPlayer)
             musicPlayer.Play();
+    }
+
+    public void PlayBulletShot()
+    {
+        sfxPlayer.PlayOneShot(bulletShotSFX);
     }
 
 }
